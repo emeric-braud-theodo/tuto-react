3cebb775a0a0985e9e93a43b0476b425cf35696e
Emeric Braud <emeric.braud@theodo.com>
2026-04-02 10:56:07 +0200

    wip

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `${process.env.FRONT_BASE_URL}:${process.env.FRONT_PORT}`,
+    origin: `${process.env.FRONT_URL}`,
   });
 
   await app.listen(process.env.PORT ?? 3000);
